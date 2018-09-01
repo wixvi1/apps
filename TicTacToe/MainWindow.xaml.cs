8f2fe97fdfdca0854aebc92490eb4f47541eade1
wixvi1 <wspirit97@inbox.lv>
2018-09-01 16:48:32 +0300

    Add files via upload

--- TicTacToe/MainWindow.xaml.cs
+++ TicTacToe/MainWindow.xaml.cs
@@ -15,11 +15,11 @@ namespace TicTacToe
     {
         #region PrivateMembers
 
-        private MarkType[] mResults;
+        private MarkType[] _results;// array of enum type
 
-        private bool mPlayer1Turn;
+        private bool _playerOneTurn;// variable for player's turn
 
-        private bool mGameEnded;
+        private bool _gameEnded;// variable to check if game is ended
         #endregion
 
         #region Constructor
@@ -30,23 +30,23 @@ namespace TicTacToe
         {
             InitializeComponent();
 
-            NewGame();
+            NewGame();// creating new game
         }
 
         #endregion
 
         /// <summary>
-        /// Start new game, clear all values
+        /// Start new game, clear all values!
         /// </summary>
         private void NewGame()
         {
             // blank array of free cells
-            mResults = new MarkType[9];
+            _results = new MarkType[9];
 
-            for (var i = 0; i < mResults.Length; i++)
-                mResults[i] = MarkType.Free;
+            for (var i = 0; i < _results.Length; i++) 
+                _results[i] = MarkType.Free;
            // Make sure its player 1 turn
-            mPlayer1Turn = true;
+            _playerOneTurn = true;
             //Itterate every button on the grid
             Container.Children.Cast<Button>().ToList().ForEach(button =>
             {
@@ -55,7 +55,7 @@ namespace TicTacToe
                 button.Foreground = Brushes.Blue;
             });
 
-            mGameEnded = false;
+            _gameEnded = false;
         }
         /// <summary>
         /// Handles the button click event
@@ -64,112 +64,112 @@ namespace TicTacToe
         /// <param name="e">The events of the click</param>
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            if (mGameEnded)
+            if (_gameEnded)
             {
                 NewGame();
                 return;
             }
 
             var button = (Button)sender;
-
             var column = Grid.GetColumn(button);
             var row = Grid.GetRow(button);
 
-            var index = column + (row * 3);
+            var index = column + (row * 3);//3 is amount of columns
+
             // Dont do anything if the cell alrady has a value in it
-            if (mResults[index] != MarkType.Free)
+            if (_results[index] != MarkType.Free)
                 return;
 
             // set the cell value 
-            mResults[index] = mPlayer1Turn ? MarkType.Cross : MarkType.Nought;
+            _results[index] = _playerOneTurn ? MarkType.Cross : MarkType.Nought;
 
             //Set buttons content
-            button.Content = mPlayer1Turn ? "X" : "O";
+            button.Content = _playerOneTurn ? "X" : "O";
 
-            if (!mPlayer1Turn)
+            if (!_playerOneTurn)
                 button.Foreground = Brushes.Green;
             // Toggle the players turn
-            mPlayer1Turn ^= true;
+            _playerOneTurn ^= true; // flips the value: if it's true - it becomes false, if it's false - it becomes true
 
             //Check for a winner
 
             CheckForWinner();
         }
-        // Check if there is a winner
+        // Check if there is a winner !
         private void CheckForWinner()
         {
             //Row 0
-            if (mResults[0] != MarkType.Free && (mResults[0] & mResults[1] & mResults[2]) == mResults[0])
+            if (_results[0] != MarkType.Free && (_results[0] & _results[1] & _results[2]) == _results[0])
             {
-                mGameEnded = true;
+                _gameEnded = true;
 
                 Button0_0.Background = Button1_0.Background = Button2_0.Background = Brushes.Indigo;
             }
 
             //Row 1
-            if (mResults[3] != MarkType.Free && (mResults[3] & mResults[4] & mResults[5]) == mResults[3])
+            if (_results[3] != MarkType.Free && (_results[3] & _results[4] & _results[5]) == _results[3])
             {
-                mGameEnded = true;
+                _gameEnded = true;
 
                 Button0_1.Background = Button1_1.Background = Button2_1.Background = Brushes.Indigo;
             }
 
             //Row 2
-            if (mResults[6] != MarkType.Free && (mResults[6] & mResults[7] & mResults[8]) == mResults[6])
+            if (_results[6] != MarkType.Free && (_results[6] & _results[7] & _results[8]) == _results[6])
             {
-                mGameEnded = true;
+                _gameEnded = true;
 
                 Button0_2.Background = Button1_2.Background = Button2_2.Background = Brushes.Indigo;
             }
 
             //Column 0
-            if (mResults[0] != MarkType.Free && (mResults[0] & mResults[3] & mResults[6]) == mResults[0])
+            if (_results[0] != MarkType.Free && (_results[0] & _results[3] & _results[6]) == _results[0])
             {
-                mGameEnded = true;
+                _gameEnded = true;
 
                 Button0_0.Background = Button0_1.Background = Button0_2.Background = Brushes.Indigo;
             }
 
             //Column 1
-            if (mResults[1] != MarkType.Free && (mResults[1] & mResults[4] & mResults[7]) == mResults[1])
+            if (_results[1] != MarkType.Free && (_results[1] & _results[4] & _results[7]) == _results[1])
             {
-                mGameEnded = true;
+                _gameEnded = true;
 
                 Button1_0.Background = Button1_1.Background = Button1_2.Background = Brushes.Indigo;
             }
 
             //Column 2
-            if (mResults[2] != MarkType.Free && (mResults[2] & mResults[5] & mResults[8]) == mResults[2])
+            if (_results[2] != MarkType.Free && (_results[2] & _results[5] & _results[8]) == _results[2])
             {
-                mGameEnded = true;
+                _gameEnded = true;
 
                 Button2_0.Background = Button2_1.Background = Button2_2.Background = Brushes.Indigo;
             }
 
             //Cross 1
-            if (mResults[0] != MarkType.Free && (mResults[0] & mResults[4] & mResults[8]) == mResults[0])
+            if (_results[0] != MarkType.Free && (_results[0] & _results[4] & _results[8]) == _results[0])
             {
-                mGameEnded = true;
+                _gameEnded = true;
 
                 Button0_0.Background = Button1_1.Background = Button2_2.Background = Brushes.Indigo;
             }
 
             //Cross 2
-            if (mResults[2] != MarkType.Free && (mResults[2] & mResults[4] & mResults[6]) == mResults[2])
+            if (_results[2] != MarkType.Free && (_results[2] & _results[4] & _results[6]) == _results[2])
             {
-                mGameEnded = true;
+                _gameEnded = true;
 
                 Button2_0.Background = Button1_1.Background = Button0_2.Background = Brushes.Indigo;
             }
 
-            if (!mGameEnded)
+            if (!_gameEnded)// if it is a draw then all becomes orange
             {
-                if (!mResults.Any(f => f == MarkType.Free))
+                if (!_results.Any(f => f == MarkType.Free))
                 {
-                    mGameEnded = true;
+                    _gameEnded = true;
 
                     Container.Children.Cast<Button>().ToList()
-                        .ForEach(button => { button.Background = Brushes.Orange; });
+                        .ForEach(button =>  button.Background = Brushes.Orange);
                 }
             }
         }
